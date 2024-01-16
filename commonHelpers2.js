f0e4a1b4014bfc253fb63cb123029b3325fe1e7f
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-01-16 00:12:05 +0000

    Deploying to gh-pages from @ YagovkinDmytro/goit-advancedjs-hw-01@77158a2fe22bab7957233858ae405cb93d003ddc 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as o}from"./assets/vendor-443c30ec.js";const m=document.querySelector("iframe"),e=new o(m);let t="videoplayer-current-time";e.on("timeupdate",i);function i({seconds:r}){localStorage.setItem(t,`${r}`)}const a=localStorage.getItem(t);e.setCurrentTime(Number(a));
+//# sourceMappingURL=commonHelpers2.js.map
