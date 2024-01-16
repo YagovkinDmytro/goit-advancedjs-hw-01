77158a2fe22bab7957233858ae405cb93d003ddc
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-01-16 01:11:32 +0100

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+let currentTimeKey = 'videoplayer-current-time';
+
+player.on('timeupdate', onTimeupdate);
+
+function onTimeupdate({ seconds }) {
+  localStorage.setItem(currentTimeKey, `${seconds}`);
+}
+
+const videoPlayerCurrentTime = localStorage.getItem(currentTimeKey);
+player.setCurrentTime(Number(videoPlayerCurrentTime));
