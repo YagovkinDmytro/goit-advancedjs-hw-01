1c4f224d564909ecd264d27a90c84be40d9b7156
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-01-21 19:13:18 +0100

    form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,33 +1,33 @@
 import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', handleEvent);
-form.addEventListener('submit', handleEvent);
+const STORAGE_KEY = 'feedback-form-state';
 
-function handleEvent(event) {
-  event.preventDefault();
+form.addEventListener('input', throttle(handleFormInput, 500));
+form.addEventListener('submit', handleFormSubmit);
 
-  let valueEmail = form.elements.email.value;
-  let valueMessage = form.elements.message.value;
-  const feedbackFormKey = 'feedback-form-state';
-  const savedFeedbackForm = localStorage.getItem(feedbackFormKey);
-  const feedbackFormObj = {
-    valueEmail,
-    valueMessage,
-  };
+const { email, message } = form.elements;
 
+let feedbackFormObj = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? {};
+
+handleFormRestoration();
+
+function handleFormInput(event) {
+  feedbackFormObj[event.target.name] = event.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormObj));
+}
+
+function handleFormRestoration() {
+  const savedFeedbackForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
   if (savedFeedbackForm) {
-    const parsedFeedbackForm = JSON.parse(savedFeedbackForm);
-    valueEmail = parsedFeedbackForm.valueEmail;
-    valueMessage = parsedFeedbackForm.valueMessage;
-    console.log(valueEmail);
-    console.log(valueMessage);
+    email.value = savedFeedbackForm.email ?? '';
+    message.value = savedFeedbackForm.message ?? '';
   }
+}
 
-  localStorage.setItem(feedbackFormKey, JSON.stringify(feedbackFormObj));
-
-  // if (event.target.nodeName !== 'BUTTON') {
-  //   return;
-  // } else {
-  // }
+function handleFormSubmit(event) {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
 }
